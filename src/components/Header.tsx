1e8688692808031b2ca5079c8b8d92004decb881
unknown <jihyeseo0801@gmail.com>
2026-07-21 00:15:36 +0900

    260721.00:15

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -51,129 +51,128 @@ const Header = ({ isLoggedIn, onLogout }: HeaderProps) => {
         setSearchValue(""); // 모달 닫힐 때 작성된 내용 초기화
     };
 
-    
-
     return (
-    <div id='header' className={`${isDarkSection ? 'theme-dark' : 'theme-light'} ${isMenuOpen ? 'menu-open' : ''}`}>
-        <div className='header_inner'>
-        <div>
-            <Link to='/'>
-            <img src={isDarkSection ? "/media/etc/logo_w.png" : "/media/etc/logo_b.png"} alt="logo" className='logo' />
-            </Link>
+        <div id='header' className={`${isDarkSection ? 'theme-dark' : 'theme-light'} ${isMenuOpen ? 'menu-open' : ''}`}>
+            <div className='header_inner'>
+                <div>
+                    <Link to='/'>
+                        <img src={isDarkSection ? "/media/etc/logo_w.png" : "/media/etc/logo_b.png"} alt="logo" className='logo' />
+                    </Link>
+                </div>
+
+                <nav className={`nav_container ${isMenuOpen ? 'active' : ''}`}>
+                    <Link to="/" className="mobile_logo_text" onClick={() => setIsMenuOpen(false)}>
+                        DIRECTORY.M
+                    </Link>
+
+                    <ul className='gnb1'>
+                        <li className={`gnbH ${openSubMenu === 'curation' ? 'active' : ''}`} onClick={() => toggleSubMenu('curation')}>
+                        <Link to='/curation' onClick={(e) => window.innerWidth > 768 ? null : e.preventDefault()}>CURATION</Link>
+                        <div className='sub_gnb'>
+                            <Link to='/curation' className="go_page_link" onClick={() => setIsMenuOpen(false)}>To Curation Space</Link>
+                            <div className='sub'><Link to='/curation?section=0'>Entrance</Link></div>
+                            <div className='sub'><Link to='/curation?section=1'>Prologue</Link></div>
+                            <div className='sub'><Link to='/curation?section=2'>Room No.01</Link></div>
+                            <div className='sub'><Link to='/curation?section=3'>Collection</Link></div>
+                            <div className='sub'><Link to='/curation?section=4'>Emblems</Link></div>
+                            <div className='sub'><Link to='/curation?section=5'>Observatory</Link></div>
+                            <div className='sub'><Link to='/curation?section=6'>Index</Link></div>
+                        </div>
+                    </li>
+                    <li className={`gnbH ${openSubMenu === 'explore' ? 'active' : ''}`} onClick={() => toggleSubMenu('explore')}>
+                        <Link to='/explore' onClick={(e) => window.innerWidth > 768 ? null : e.preventDefault()}>EXPLORE</Link>
+                        <div className='sub_gnb'>
+                            <Link to='/explore' className="go_page_link" onClick={() => setIsMenuOpen(false)}>To Explore Space</Link>
+                            <div className='sub'><Link to='/explore?section=0'>Intro</Link></div>
+                            <div className='sub'><Link to='/explore?section=1'>Exhibition</Link></div>
+                            <div className='sub'><Link to='/explore?section=3'>All Kinds of Cinema</Link></div>
+                            <div className='sub'><Link to='/explore?section=4'>Discovery</Link></div>
+                        </div>
+                    </li>
+                    <li className={`gnbH ${openSubMenu === 'funding' ? 'active' : ''}`} onClick={() => toggleSubMenu('funding')}>
+                        <Link to='/funding' onClick={(e) => window.innerWidth > 768 ? null : e.preventDefault()}>FUNDING</Link>
+                        <div className='sub_gnb'>
+                            <Link to='/funding' className="go_page_link" onClick={() => setIsMenuOpen(false)}>To Funding Space</Link>
+                            <div className='sub'><Link to='/funding?section=0'>Final Piece</Link></div>
+                            <div className='sub'><Link to='/funding?section=1'>Take #01</Link></div>
+                            <div className='sub'><Link to='/funding?section=2'>Live Projects</Link></div>
+                            <div className='sub'><Link to='/funding?section=3'>Values</Link></div>
+                            <div className='sub'><Link to='/funding?section=4'>Funding Report</Link></div>
+                        </div>
+                    </li>
+                </ul>
+
+                <ul className='gnb2'>
+                    <li className='menu_item lang_item'>
+                        <div className="lang_toggle_wrap">
+                            <button 
+                                onClick={() => i18n.changeLanguage('ko')} 
+                                className={`lang_toggle_btn ${i18n.language.includes('ko') ? 'active' : ''}`}
+                            >
+                                KOR
+                            </button>
+                            <span className="lang_divider">/</span>
+                            <button 
+                                onClick={() => i18n.changeLanguage('en')} 
+                                className={`lang_toggle_btn ${i18n.language.includes('en') ? 'active' : ''}`}
+                            >
+                                ENG
+                            </button>
+                        </div>
+                    </li>
+
+                    <li className={`menu_item ${openSubMenu === 'user' ? 'active' : ''}`} onClick={() => toggleSubMenu('user')}>
+                        <Link to='#' onClick={(e) => window.innerWidth <= 768 && e.preventDefault()}>
+                            <img src="/media/etc/userL_b.svg" alt="user" />
+                        </Link>
+                        <div className='sub_menu'>
+                            {isLoggedIn ? (
+                                <>
+                                    <div className='sub'><Link to='/' onClick={onLogout}>LOGOUT</Link></div>
+                                    <div className='sub'><Link to='/mypage' onClick={() => setIsMenuOpen(false)}>MY SPACE</Link></div>
+                                    <div className='sub'><Link to='/membership' onClick={() => setIsMenuOpen(false)}>MEMBERSHIP</Link></div>
+                                </>
+                            ) : (
+                                <>
+                                    <div className='sub'><Link to='/login' onClick={() => setIsMenuOpen(false)}>LOGIN</Link></div>
+                                    <div className='sub'><Link to='/signup' onClick={() => setIsMenuOpen(false)}>SIGN UP</Link></div>
+                                    <div className='sub'><Link to='/membership' onClick={() => setIsMenuOpen(false)}>MEMBERSHIP</Link></div>
+                                </>
+                            )}
+                        </div>
+                    </li>
+                    <li className='menu_item'>
+                        <button onClick={() => setIsSearchOpen(true)} className="search_btn">
+                            <img src={isDarkSection ? "/media/etc/searchL_w.svg" : "/media/etc/searchL_b.svg"} alt="search" />
+                        </button>
+
+                        {isSearchOpen && (
+                            <div className={`search_modal_overlay ${isSearchOpen ? 'active' : ''}`}
+                                 onClick={handleCloseSearch}>
+                                <div className='search_modal_content' onClick={(e) => e.stopPropagation()}>
+                                    <div className='search_input_wrapper'>
+                                        <input type='text' autoFocus={isSearchOpen} value={searchValue}
+                                               onChange={(e) => setSearchValue(e.target.value)} />
+                                        <p className='he_ph' onClick={handleCloseSearch}>Search</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    </li>
+                </ul>
+
+                <div className='mobile_search_bar'>
+                        <input type='text' value={searchValue} onChange={(e) => setSearchValue(e.target.value)}/>
+                        <p className='he_ph' onClick={() => setSearchValue("")}>Search</p>
+                </div>
+                </nav>
+
+                <button className="mobile_menu_btn" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                    <img src="/media/etc/arrow_b.svg" alt="menu toggle" />
+                </button>
+            </div>
+            {isMenuOpen && <div className="menu_overlay" onClick={() => setIsMenuOpen(false)}></div>}
         </div>
-
-        <nav className={`nav_container ${isMenuOpen ? 'active' : ''}`}>
-            <Link to="/" className="mobile_logo_text" onClick={() => setIsMenuOpen(false)}>
-            DIRECTORY.M
-            </Link>
-
-            <ul className='gnb1'>
-            <li className={`gnbH ${openSubMenu === 'curation' ? 'active' : ''}`} onClick={() => toggleSubMenu('curation')}>
-                <Link to='/curation' onClick={(e) => window.innerWidth > 768 ? null : e.preventDefault()}>CURATION</Link>
-                <div className='sub_gnb'>
-                <Link to='/curation' className="go_page_link" onClick={() => setIsMenuOpen(false)}>To Curation Space</Link>
-                <div className='sub'><Link to='/curation?section=0'>Entrance</Link></div>
-                <div className='sub'><Link to='/curation?section=1'>Prologue</Link></div>
-                <div className='sub'><Link to='/curation?section=2'>Room No.01</Link></div>
-                <div className='sub'><Link to='/curation?section=3'>Collection</Link></div>
-                <div className='sub'><Link to='/curation?section=4'>Emblems</Link></div>
-                <div className='sub'><Link to='/curation?section=5'>Observatory</Link></div>
-                <div className='sub'><Link to='/curation?section=6'>Index</Link></div>
-                </div>
-            </li>
-            <li className={`gnbH ${openSubMenu === 'explore' ? 'active' : ''}`} onClick={() => toggleSubMenu('explore')}>
-                <Link to='/explore' onClick={(e) => window.innerWidth > 768 ? null : e.preventDefault()}>EXPLORE</Link>
-                <div className='sub_gnb'>
-                <Link to='/explore' className="go_page_link" onClick={() => setIsMenuOpen(false)}>To Explore Space</Link>
-                <div className='sub'><Link to='/explore?section=0'>Intro</Link></div>
-                <div className='sub'><Link to='/explore?section=1'>Exhibition</Link></div>
-                <div className='sub'><Link to='/explore?section=3'>All Kinds of Cinema</Link></div>
-                <div className='sub'><Link to='/explore?section=4'>Discovery</Link></div>
-                </div>
-            </li>
-            <li className={`gnbH ${openSubMenu === 'funding' ? 'active' : ''}`} onClick={() => toggleSubMenu('funding')}>
-                <Link to='/funding' onClick={(e) => window.innerWidth > 768 ? null : e.preventDefault()}>FUNDING</Link>
-                <div className='sub_gnb'>
-                <Link to='/funding' className="go_page_link" onClick={() => setIsMenuOpen(false)}>To Funding Space</Link>
-                <div className='sub'><Link to='/funding?section=0'>Final Piece</Link></div>
-                <div className='sub'><Link to='/funding?section=1'>Take #01</Link></div>
-                <div className='sub'><Link to='/funding?section=2'>Live Projects</Link></div>
-                <div className='sub'><Link to='/funding?section=3'>Values</Link></div>
-                <div className='sub'><Link to='/funding?section=4'>Funding Report</Link></div>
-                </div>
-            </li>
-            </ul>
-
-            
-            <ul className='gnb2'>
-            <div className="header_left_fixed">
-                <div className="lang_toggle_wrap">
-                <button 
-                    onClick={() => i18n.changeLanguage('ko')} 
-                    className={`lang_toggle_btn ${i18n.language.includes('ko') ? 'active' : ''}`}
-                >
-                    KOR
-                </button>
-                <span className="lang_divider">/</span>
-                <button 
-                    onClick={() => i18n.changeLanguage('en')} 
-                    className={`lang_toggle_btn ${i18n.language.includes('en') ? 'active' : ''}`}
-                >
-                    ENG
-                </button>
-                </div>
-            </div>
-            <li className={`menu_item ${openSubMenu === 'user' ? 'active' : ''}`} onClick={() => toggleSubMenu('user')}>
-                <Link to='#' onClick={(e) => window.innerWidth <= 768 && e.preventDefault()}>
-                <img src={isDarkSection ? "/media/etc/userL_w.svg" : "/media/etc/userL_b.svg"} alt="user" />
-                </Link>
-                <div className='sub_menu'>
-                {isLoggedIn ? (
-                    <>
-                    <div className='sub'><Link to='/' onClick={onLogout}>LOGOUT</Link></div>
-                    <div className='sub'><Link to='/mypage' onClick={() => setIsMenuOpen(false)}>MY SPACE</Link></div>
-                    <div className='sub'><Link to='/membership' onClick={() => setIsMenuOpen(false)}>MEMBERSHIP</Link></div>
-                    </>
-                ) : (
-                    <>
-                    <div className='sub'><Link to='/login' onClick={() => setIsMenuOpen(false)}>LOGIN</Link></div>
-                    <div className='sub'><Link to='/signup' onClick={() => setIsMenuOpen(false)}>SIGN UP</Link></div>
-                    <div className='sub'><Link to='/membership' onClick={() => setIsMenuOpen(false)}>MEMBERSHIP</Link></div>
-                    </>
-                )}
-                </div>
-            </li>
-            <li className='menu_item pc_only_search'>
-                <button onClick={() => setIsSearchOpen(true)} className="search_btn">
-                <img src={isDarkSection ? "/media/etc/searchL_w.svg" : "/media/etc/searchL_b.svg"} alt="search" />
-                </button>
-            </li>
-            </ul>
-
-            <div className='mobile_search_bar'>
-            <input type='text' value={searchValue} onChange={(e) => setSearchValue(e.target.value)}/>
-            <p className='he_ph' onClick={() => setSearchValue("")}>Search</p>
-            </div>
-        </nav>
-
-        {/* 모달 검색창 유지 */}
-        {isSearchOpen && (
-            <div className={`search_modal_overlay ${isSearchOpen ? 'active' : ''}`} onClick={handleCloseSearch}>
-            <div className='search_modal_content' onClick={(e) => e.stopPropagation()}>
-                <div className='search_input_wrapper'>
-                <input type='text' autoFocus={isSearchOpen} value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
-                <p className='he_ph' onClick={handleCloseSearch}>Search</p>
-                </div>
-            </div>
-            </div>
-        )}
-
-        <button className="mobile_menu_btn" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-            <img src={isDarkSection ? "/media/etc/arrow_w.svg" : "/media/etc/arrow_b.svg"} alt="menu toggle" />
-        </button>
-        </div>
-        {isMenuOpen && <div className="menu_overlay" onClick={() => setIsMenuOpen(false)}></div>}
-    </div>
     );
 };
 
